f4d574ac281c7435bc7fe34cb5fb4408790de2d8
kttling2005 <80590767+kttling2005@users.noreply.github.com>
2026-09-21 20:48:15 +0700

    Add Laptop class implementing Serializable

---
+++ TCP_ObjectStream/TCP/Laptop.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TCP;
+
+/**
+ *
+ * @author bugbug05
+ */
+import java.io.Serializable;
+
+public class Laptop implements Serializable{
+    private static final long serialVersionUID = 20150711L;
+
+    private int id;
+    private String code;
+    private String name;
+    private int quantity;
+
+    public Laptop(int id, String code, String name, int quantity) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
