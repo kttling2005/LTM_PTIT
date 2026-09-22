a2e5c0a573b52b59079e4910c9e27dabd4582e38
kttling2005 <80590767+kttling2005@users.noreply.github.com>
2026-09-22 10:41:01 +0700

    Create Customer.java

---
+++ TCP_ObjectStream2/TCP/Customer.java
@@ -0,0 +1,56 @@
+package TCP;
+import java.io.*;
+public class Customer implements Serializable{
+    private static final long serialVersionUID = 20170711L;
+    
+    private int id;
+    private String code;
+    private String name;
+    private String dayOfBirth;
+    private String userName;
+    
+    public Customer(int id, String code, String name, String dayOfBirth, String userName){
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.dayOfBirth = dayOfBirth;
+        this.userName = userName;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public String getCode(){
+        return code;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getDayOfBirth(){
+        return dayOfBirth;
+    }
+    
+    public String getUserName(){
+        return userName;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public void setDayOfBirth(String dayOfBirth){
+        this.dayOfBirth = dayOfBirth;
+    }
+    
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+    
+}
