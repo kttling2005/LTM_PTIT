4bd8e3b1e8a6f646f97a0dde272291bb20bd9e79
kttling2005 <80590767+kttling2005@users.noreply.github.com>
2026-09-22 12:05:09 +0700

    Refactor socket communication in main method
    Refactored the main method to streamline socket communication and calculations.

--- TCP_DataStream1.java
+++ TCP_DataStream1.java
@@ -1,51 +1,27 @@
 package javaapplication1;
-
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class JavaApplication1 {
 
     public static void main(String[] args) throws IOException {
-
-        String serverHost = "36.50.135.242";
-        int serverPort = 2207;
-
-        String studentCode = "B23DCCN483";
-        String qCode = "zviAFgcl";
-
-        Socket socket = new Socket(serverHost, serverPort);
-
-        DataInputStream in = new DataInputStream(
-                socket.getInputStream());
-
-        DataOutputStream out = new DataOutputStream(
-                socket.getOutputStream());
-
-        // a. Gửi mã sinh viên và mã câu hỏi
-        String request = studentCode + ";" + qCode;
-        out.writeUTF(request);
-        out.flush();
-
-        // b. Nhận hai số nguyên a và b
-        int a = in.readInt();
-        int b = in.readInt();
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-
-        // c. Tính tổng và tích
-        int sum = a + b;
-        int product = a * b;
-
-        // Gửi lần lượt tổng rồi tích
-        out.writeInt(sum);
-        out.writeInt(product);
-        out.flush();
-
-        System.out.println("Sum = " + sum);
-        System.out.println("Product = " + product);
-
-        // d. Đóng kết nối
+        Socket socket = new Socket("36.50.135.242",2207);
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        
+        dos.writeUTF("B23DCCN483;zviAFgcl");
+        dos.flush();
+        
+        int a = dis.readInt();
+        int b = dis.readInt();
+        
+        dos.writeInt(a+b);
+        dos.writeInt(a*b);
+        dos.flush();
+        
+        dos.close();
+        dis.close();
         socket.close();
     }
 }
