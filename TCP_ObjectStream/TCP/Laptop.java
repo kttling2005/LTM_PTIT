8629d5bdd47b65b119f1ed15bc69fea5756f0191
kttling2005 <80590767+kttling2005@users.noreply.github.com>
2026-09-22 08:36:24 +0700

    Update Laptop.java

--- TCP_ObjectStream/TCP/Laptop.java
+++ TCP_ObjectStream/TCP/Laptop.java
@@ -1,59 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TCP;
-
-/**
- *
- * @author bugbug05
- */
-import java.io.Serializable;
+import java.io.*;
 
 public class Laptop implements Serializable{
+    
     private static final long serialVersionUID = 20150711L;
-
+    
     private int id;
     private String code;
     private String name;
     private int quantity;
-
-    public Laptop(int id, String code, String name, int quantity) {
+    
+    public Laptop(int id, String code, String name, int quantity){
         this.id = id;
         this.code = code;
         this.name = name;
         this.quantity = quantity;
     }
-
-    public int getId() {
+    
+    public int getId(){
         return id;
     }
-
-    public String getCode() {
+    
+    public String getCode(){
         return code;
     }
-
-    public String getName() {
+    
+    public String getName(){
         return name;
     }
-
-    public int getQuantity() {
+    
+    public int getQuantity(){
         return quantity;
     }
-
-    public void setId(int id) {
+    
+    public void setId(int id){
         this.id = id;
     }
-
-    public void setCode(String code) {
+    
+    public void setCode(String code){
         this.code = code;
     }
-
-    public void setName(String name) {
+    
+    public void setName(String name){
         this.name = name;
     }
-
-    public void setQuantity(int quantity) {
+    
+    public void setQuantity(int quantity){
         this.quantity = quantity;
     }
 }
